19a44c968fd50f9de825b480c95d3db52c7a70cc
tdelubac <timothee.delubac@gmail.com>
2016-10-27 13:36:58 +0200

    1) Modified data.h to return const reference to member functions 2) added some code to main to read example 3) removed header of example file examples/angular_correlation_in_pixels.txt

--- src/libs/data.h
+++ src/libs/data.h
@@ -8,89 +8,89 @@
 template<class TYPE>
 class Data
 {
-private:
-  std::vector<TYPE> m_x;
-  std::vector<TYPE> m_y;
-  std::vector<TYPE> m_z;
-  std::vector<TYPE> m_val;
-  std::vector<TYPE> m_weight;
-  std::vector<size_t> m_sample;
-public:
-  // constructors
-  Data() {}
-  Data(std::string source, size_t dim) {this->load(source,dim);} 
-  // getter functions
-  std::vector<TYPE> x() const
-  {
-    return m_x;
-  }
-  std::vector<TYPE> y() const
-  {
-    return m_y;
-  }
-  std::vector<TYPE> z() const
-  {
-    return m_z;
-  }
-  std::vector<TYPE> val() const
-  {
-    return m_val;
-  }
-  std::vector<TYPE> weight() const
-  {
-    return m_weight;
-  }
-  std::vector<size_t> sample() const
-  {
-    return m_sample;
-  }
-  // loading data
-  void load(std::string source, size_t dim)
-  /*
-    Load source into class data.
-    source must be a 5 columns (dim=2) or 6 columns (dim=3) file.
-    First columns are assume to be x,y(,z).
-    third to last column assume to be value at x,y(,z) position.
-    Second to last column assume to be weight.
-    Last column assume to be sample number. Must be an integer!
-   */
-  {
-    if((dim!=2) & (dim!=3))
-      {
-	throw std::domain_error("Data::load(std::string source, size_t dim): dim must be 2 or 3.");
-	return;
-      }
-    std::ifstream file(source);
-    if(!file.is_open())
-      {
-	throw std::runtime_error("Data::load(std::string source, size_t dim): could not open source file.");
-      }
-    // checking size of file  
-    size_t n_lines = 0;
-    std::string line;
-    while (std::getline(file, line))
-      ++n_lines;
-    // going back to beginning of file
-    file.clear();
-    file.seekg(0, std::ios::beg);
-    // setting size of vectors
-    m_x.resize(n_lines);
-    m_y.resize(n_lines);
-    m_z.resize(n_lines);
-    m_val.resize(n_lines);
-    m_weight.resize(n_lines);
-    m_sample.resize(n_lines);
-    // loading source to data
-    for (int irow=0; irow<n_lines; irow++){
-      file >> m_x[irow];
-      file >> m_y[irow];
-      if(dim == 3) file >> m_z[irow];
-      file >> m_val[irow];
-      file >> m_weight[irow];
-      file >> m_sample[irow];
+ private:
+    std::vector<TYPE> m_x;
+    std::vector<TYPE> m_y;
+    std::vector<TYPE> m_z;
+    std::vector<TYPE> m_val;
+    std::vector<TYPE> m_weight;
+    std::vector<size_t> m_sample;
+ public:
+    // constructors
+    Data() {}
+    Data(std::string source, size_t dim) {this->load(source,dim);} 
+    // getter functions returning constant reference to member functions
+    const std::vector<TYPE>  & x()
+    {
+        return m_x;
     }
-    return;
-  }
+    const std::vector<TYPE> & y()
+    {
+        return m_y;
+    }
+    const std::vector<TYPE> & z()
+    {
+        return m_z;
+    }
+    const std::vector<TYPE> & val()
+    {
+        return m_val;
+    }
+    const std::vector<TYPE> & weight()
+    {
+        return m_weight;
+    }
+    const std::vector<size_t> & sample()
+    {
+        return m_sample;
+    }
+    // loading data
+    void load(std::string source, size_t dim)
+    /*
+      Load source into class data.
+      source must be a 5 columns (dim=2) or 6 columns (dim=3) file.
+      First columns are assume to be x,y(,z).
+      third to last column assume to be value at x,y(,z) position.
+      Second to last column assume to be weight.
+      Last column assume to be sample number. Must be an integer!
+    */
+    {
+        if((dim!=2) & (dim!=3))
+            {
+                throw std::domain_error("Data::load(std::string source, size_t dim): dim must be 2 or 3.");
+                return;
+            }
+        std::ifstream file(source);
+        if(!file.is_open())
+            {
+                throw std::runtime_error("Data::load(std::string source, size_t dim): could not open source file.");
+            }
+        // checking size of file  
+        size_t n_lines = 0;
+        std::string line;
+        while (std::getline(file, line))
+            ++n_lines;
+        // going back to beginning of file
+        file.clear();
+        file.seekg(0, std::ios::beg);
+        // setting size of vectors
+        m_x.resize(n_lines);
+        m_y.resize(n_lines);
+        m_z.resize(n_lines);
+        m_val.resize(n_lines);
+        m_weight.resize(n_lines);
+        m_sample.resize(n_lines);
+        // loading source to data
+        for (int irow=0; irow<n_lines; irow++){
+            file >> m_x[irow];
+            file >> m_y[irow];
+            if(dim == 3) file >> m_z[irow];
+            file >> m_val[irow];
+            file >> m_weight[irow];
+            file >> m_sample[irow];
+        }
+        return;
+    }
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+#include <stdexcept>
 #include <iostream>
+
+#include "libs/data.h"
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+    Data<float> myData;
+    try{
+        myData.load("../examples/angular_correlation_in_pixels.txt",2);
+    }catch(std::exception &err){
+        std::cout << err.what() << "\n";
+    }
+    size_t i = 1;
+    std::cout << myData.x()[i] << " " << myData.y()[i] << " " << myData.z()[i] << " " << myData.val()[i] << " " << myData.weight()[i] << " " << myData.sample()[i] << "\n";
 }
